d2e255b16c7859bbac4e5a1de94853e0452fa710
graphicdesigndropout <iamtsofu@gmail.com>
2020-10-10 16:20:03 -0700

    Updated daily bot with flu shot reminder Added .gitignore

--- inspirobot_bot_daily.py
+++ inspirobot_bot_daily.py
@@ -4,12 +4,23 @@ import time
 import telegram
 import os
 
+botID = os.environ.get('BOT_TOKEN')
+bot = telegram.Bot(token=botID)
+
+roomID = os.environ.get('ROOMID')
+
 def goodmorning():
     r = requests.get('https://inspirobot.me/api?generate=true')
-    bot = telegram.Bot(token=os.environ.get('ROOMID'))
-    bot.sendMessage(chat_id =os.environ.get('BOT_TOKEN'), text = f'Good morning!\n {r.text}')
+    bot.sendMessage(chat_id=roomID, text = f'Good morning!\n {r.text}')
+
+def flushot():
+    bot.sendMessage(chat_id=roomID, text="Have you received your flu shot?\
+    \n\nFind a location near you: https://vaccinefinder.org/find-vaccine\
+    \n\nHave questions or concerns about the flu vaccine? Find more information here: https://www.cdc.gov/flu/season/faq-flu-season-2020-2021.htm")
+
 
 schedule.every().day.at('04:20').do(goodmorning)
+schedule.every().day.at('12:00').do(flushot)
 
 while True:
     schedule.run_pending()
